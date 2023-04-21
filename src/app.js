8ddc92d9224aa17ce5a7dc193c68f087b7eeff09
Kevin Bourgeois <kevin.bourgeois@heig-vd.ch>
2023-04-21 10:12:35 +0200

    zoom graph v.1

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import {getData} from "./api";
-import {select, selectAll} from "d3-selection";
-import {scaleBand, scaleLinear, scaleOrdinal} from "d3-scale";
+import {select} from "d3-selection";
+import {scaleBand, scaleLinear} from "d3-scale";
 import {axisBottom, axisLeft} from "d3-axis";
-import {descending} from "d3-array";
-import {transition, interrupt} from "d3-transition";
-import data from "d3-selection/src/selection/data";
+import * as d3 from 'd3'
+import {zoom} from "d3";
+import band from "d3-scale/src/band";
+import {event as d3event} from'd3-selection'
+const templatePopUp = document.querySelector('#template-pop-up')
 
 getData.then(episodes => {
 
-    const POPUP_WIDTH = 300
-    const POPUP_HEIGHT = 150
-
-    const svg = select("#graph")
+    //Dessin du graph
+    var svg = select("#graph")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -19,117 +19,108 @@ getData.then(episodes => {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-    const arrNoEpisode = episodes.map(episode => episode.noEpisodeOverall);
+    //Juste le numéro de tout les épisodes
+    var arrNoEpisode = episodes.map(episode => episode.noEpisodeOverall);
+
+
 
     // Echelle
-    const bandScale = scaleBand()
+    var bandScale = scaleBand()
         .domain(arrNoEpisode)
         .range([0, width])
         .paddingInner(0.5);
 
-    const linearScale = scaleLinear()
+    var linearScale = scaleLinear()
         .domain([0, 15])
         .range([height, 0])
 
-    //Axes
-    const xAxis = (g) => {
 
-        g.attr("transform", `translate(0, ${height})`)
-            .call(axisBottom(bandScale).tickFormat(i => episodes[i].noEpisodeOverall))
+    var xAxis = svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .attr("class", "x-axis")
+        .call(d3.axisBottom(bandScale));
 
-    }
+    var yAxis = svg.append("g")
+        .attr("class", "y-axis")
+        .call(d3.axisLeft(linearScale));
+
+    var line = d3.line()
+        .x(function (d, i) {
+            return bandScale(d.noEpisodeOverall);
+        })
+        .y(function (d) {
+            return linearScale(d.viewers);
+        });
+
+    //Dessine la ligne du graph
+    svg.append('path')
+        .datum(episodes)
+        .attr('d', line)
+        .attr('fill', 'none')
+        .attr('stroke', 'blue')
+        .attr('stroke-width', 3)
+        .attr("class", "line-graph")
 
 
-    const yAxis = (g) => {
-        //g.call(axisLeft(linearScale).ticks(episodes.viewers.sort((a,b) => descending(a.viewers, b.viewers))));
-        g.call(axisLeft(linearScale).ticks());
-    }
-
+    //Dessine les points du graph
     svg
         .selectAll("rect")
         .data(episodes)
-        .join(enter => enter.append("rect")
-            .attr("x", (d) => bandScale(d.noEpisodeOverall))
-            .attr("y", (d) => linearScale(d.viewers))
-            .attr("width", bandScale.bandwidth())
-            .attr("height", (d) =>  linearScale(0) - linearScale(d.viewers))
-            .attr("fill", "royalblue")
+        .join(enter => enter.append("circle")
+            .attr('r', 5)
+            .attr("cx", (d) => bandScale(d.noEpisodeOverall))
+            .attr("cy", (d) => linearScale(d.viewers))
+            .attr("fill", "blue")
+            .attr("class", "dot")
             .on("mouseover", (e, d) => {
-
                 const title = d.title
-                const synopsis = d.synopsis
 
-                const rect = e.target;
+                const copyPopUp = templatePopUp.content.cloneNode(true)
 
-                const yB = +rect.getAttribute("y")
-                const xB = +rect.getAttribute("x")
+                document.querySelector('body').append(copyPopUp)
 
-                const lB = +rect.getAttribute("width")
+                const popUp = document.querySelector('#pop-up')
 
+                popUp.style.left = e.pageX + "px"
+                popUp.style.top = e.pageY - 200 + "px"
 
-                const popUp = svg.append("rect")
-
-                    //Création de la pop up
-
-                    .classed("bulle", true)
-                    .attr("width", POPUP_WIDTH)
-                    .attr("height", POPUP_HEIGHT)
-                    .attr("x", (xB - POPUP_WIDTH/2) +lB)
-                    .attr("y", yB-POPUP_HEIGHT*1.5)
-                    .style("fill", "red")
-                    .attr("opacity", 1)
-
-
-                svg.append("text")
-                    .classed("textClass", true)
-                    .attr("x", popUp.attr("x"))
-                    .attr("y", popUp.attr("y")-50)
-                    .text(title)
-
-
-
-                svg.append("text")
-                    .classed("textClass", true)
-                    .attr("x", +popUp.attr("x") + popUp.attr('width')/2)
-                    .attr("y", popUp.attr("y"))
-
-                    /*.attr("text-anchor", "middle")
-                    .attr("dominant-baseline", "central")
-                    .attr("textLength", popUp.attr("width")*.8)
-                    .attr("legthAdjust", "spacingAndGlyphs")*/
-                    .text(synopsis)
-
-
-
-
-
-                /*                   .transition()
-                                   .duration(500)
-                                   .attr("opacity", 1)
-                                   .interrupt()
-               */
-
-
-
-
+                //Mets les données dans la popup
+                popUp.querySelector('h2').textContent = title
 
             })
             .on("mouseout", () => {
-                selectAll("rect.bulle").remove()
-                selectAll(".textClass").remove()
+                document.getElementById('pop-up').remove()
             })
 
 )
 
+    const zoom = d3.zoom()
+        .scaleExtent([1, 5])
+        .on("zoom", zoomed);
 
-    svg.append('g').call(yAxis)
-    svg.append('g').call(xAxis)
+    function initZoom() {
+        d3.select('svg')
+            .call(zoom);
+    }
+
+    function zoomed({ transform }) {
+        svg.select('.line-graph').attr("transform", transform);
+        svg.selectAll('.dot').attr("transform", transform);
+        // Régénerer un axe à chaque fois qu'on zoom
 
 
+    }
 
+    initZoom();
 
 
 })
+
+
+
+
+
+
 const margin = {top: 10, right: 40, bottom: 30, left: 40}
 const width = 1200 - margin.left - margin.right;
 
